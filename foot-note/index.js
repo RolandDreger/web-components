4a66f1de4d6c7003fbe72d752893930ea88e43e2
Roland Dreger <mail@rolanddreger.net>
2020-09-07 23:00:10 +0200

    Add whenDefined check

--- foot-note/index.js
+++ foot-note/index.js
@@ -3,25 +3,29 @@
 /* Define <foot-note> */
 window.customElements.define('foot-note', FootNote);
 
-
-/* Set index of footnotes programmatically (optional) */
-const sections = document.querySelectorAll('section');
-sections.forEach(section => {
-	const footnotes = section.querySelectorAll('foot-note');
-	footnotes.forEach((note, index) => {
-		note.setAttribute('index', index + 1);
+customElements.whenDefined('foot-note')
+.then(_ => {
+	/* Set index of footnotes programmatically (optional) */
+	const sections = document.querySelectorAll('section');
+	sections.forEach(section => {
+		const footnotes = section.querySelectorAll('foot-note');
+		footnotes.forEach((note, index) => {
+			note.setAttribute('index', index + 1);
+		});
 	});
-});
-
-/* Add Event Listener (optional) */
-sections.forEach(section => {
-	const footnotes = section.querySelectorAll('foot-note');
-	footnotes.forEach((note, index) => {
-		note.addEventListener('footnote-on-toggle', (event) => {
-			console.log("Footonote toggled. Visible: " + event.detail.visible);
-		}, false);
-		note.addEventListener('footnote-on-hide', (event) => {
-			console.log("Footonote hidden. Visible: " + event.detail.visible);
-		}, false);
+	/* Add Event Listener (optional) */
+	sections.forEach(section => {
+		const footnotes = section.querySelectorAll('foot-note');
+		footnotes.forEach((note, index) => {
+			note.addEventListener('footnote-on-toggle', (event) => {
+				console.log("Footonote toggled. Visible: " + event.detail.visible);
+			}, false);
+			note.addEventListener('footnote-on-hide', (event) => {
+				console.log("Footonote hidden. Visible: " + event.detail.visible);
+			}, false);
+		});
 	});
+})
+.catch(error => {
+	console.log(error);
 });
