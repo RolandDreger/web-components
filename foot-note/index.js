c69bba0b8483d5b90c0535e4e1a73353c34976f8
Roland Dreger <mail@rolanddreger.net>
2020-09-10 21:24:48 +0200

    Add check for customElements

--- foot-note/index.js
+++ foot-note/index.js
@@ -1,33 +1,41 @@
 ﻿import FootNote from './src/footNote.js';
 
-/* Define <foot-note> */
-window.customElements.define('foot-note', FootNote);
 
-customElements.whenDefined('foot-note')
-.then(_ => {
-	/* Set index of footnotes programmatically (optional) */
-	const sections = document.querySelectorAll('section');
-	sections.forEach(section => {
-		const footnotes = section.querySelectorAll('foot-note');
-		footnotes.forEach((note, index) => {
-			if(!note.hasAttribute('index')) {
-				note.setAttribute('index', index + 1);
-			}
+if('customElements' in window) {
+
+	/* Define <foot-note> */
+	window.customElements.define('foot-note', FootNote);
+
+	customElements.whenDefined('foot-note')
+	.then(_ => {
+
+		/* Set index of footnotes programmatically (optional) */
+		const sections = document.querySelectorAll('section');
+		sections.forEach(section => {
+			const footnotes = section.querySelectorAll('foot-note');
+			footnotes.forEach((note, index) => {
+				if(!note.hasAttribute('index')) {
+					note.setAttribute('index', index + 1);
+				}
+			});
 		});
-	});
-	/* Add Event Listener (optional) */
-	sections.forEach(section => {
-		const footnotes = section.querySelectorAll('foot-note');
-		footnotes.forEach((note, index) => {
-			note.addEventListener('footnote-on-toggle', (event) => {
-				console.log("Footnote toggled. Visible: " + event.detail.visible);
-			}, false);
-			note.addEventListener('footnote-on-hide', (event) => {
-				console.log("Footnote hidden. Visible: " + event.detail.visible);
-			}, false);
+
+		/* Add Event Listener (optional) */
+		sections.forEach(section => {
+			const footnotes = section.querySelectorAll('foot-note');
+			footnotes.forEach((note, index) => {
+				note.addEventListener('footnote-on-toggle', (event) => {
+					console.log("Footnote toggled. Visible: " + event.detail.visible);
+				}, false);
+				note.addEventListener('footnote-on-hide', (event) => {
+					console.log("Footnote hidden. Visible: " + event.detail.visible);
+				}, false);
+			});
 		});
+		
+	})
+	.catch(error => {
+		console.error(error);
 	});
-})
-.catch(error => {
-	console.log(error);
-});
+
+}
